311532859c735520de6d0aa77865932a66434231
Deybi2 <deybijuaniquina@gmail.com>
2024-04-29 12:06:47 -0300

    Trabajando en el Paso 3

--- a/__init__.py
+++ a/__init__.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
+
+with app.app.context():
+    from . import init_app
+    init_app(app)
 
 @app.route('/')
 def hello():
